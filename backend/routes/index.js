ce235c14868e92cf2d5e16af04820f88f8358d37
sergey <sapokrovsky@gmail.com>
2023-02-03 16:43:38 +0300

    way change

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,8 @@ const cardRoutes = require('./cards');
 const userRoutes = require('./users');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('/users', userRoutes);
-router.use('/cards', cardRoutes);
+router.use('/api/users', userRoutes);
+router.use('/api/cards', cardRoutes);
 router.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
